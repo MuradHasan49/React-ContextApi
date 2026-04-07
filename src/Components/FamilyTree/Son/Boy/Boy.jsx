7b0e0d37681e97d89cb154e305bf872d09fece4b
Murad Hasan <muradhasanshuvo00@gmail.com>
2026-04-07 22:09:58 +0600

    add button style

--- src/Components/FamilyTree/Son/Boy/Boy.jsx
+++ src/Components/FamilyTree/Son/Boy/Boy.jsx
@@ -11,7 +11,7 @@ const Boy = () => {
         <div>
             <h1>Boy Component</h1>
           <p>total is : {data} </p>
-          <button className='bg-red-500'  onClick={()=>setData(data+ 100)}>add money</button>
+          <button className="px-6 py-2.5 bg-indigo-600 text-white font-medium text-sm leading-tight uppercase rounded shadow-md hover:bg-indigo-700 hover:shadow-lg focus:bg-indigo-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-indigo-800 active:shadow-lg transition duration-150 ease-in-out"  onClick={()=>setData(data+ 100)}>add money</button>
         </div>
     )
 }
